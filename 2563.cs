7807616dc34d817de7cc2e86a7b5fb912a88f843
Jodyisnotavailable <124157532+Jodyisnotavailable@users.noreply.github.com>
2024-11-13 17:45:10 +0800

    2563. Count the Number of Fair Pairs

--- 2563.cs
+++ 2563.cs
@@ -3,14 +3,28 @@ public class Solution {
         long res = 0;
         Array.Sort(nums);
         
-        for (int l = 0; l < nums.Length; l ++) {
-            for (int r = nums.Length-1; r > l; r --) {
-                if (lower <= nums[r]+nums[l] && nums[r]+nums[l] <= upper) {
-                    res += 1;
-                }
-            }
+        for (int i = 0; i < nums.Length; i ++) {
+            //find the min/max value the left/right point needs to be
+            int minVal = lower - nums[i];
+            int maxVal = upper - nums[i];
+            //get the range of array between the min and max
+            res += (binSearch(i+1, nums.Length-1, maxVal+1, nums) - binSearch(i+1, nums.Length-1, minVal, nums));
+            
         }
 
         return res;
     }
+
+    private int binSearch(int lPt, int rPt, int target, int[] nums) {
+        while (lPt <= rPt) {
+            int mPt = lPt + (rPt-lPt)/2;
+            if (nums[mPt] >= target) {
+                rPt = mPt - 1;
+            } else {
+                lPt = mPt + 1;
+            }
+        }
+        return rPt;
+    }
+    
 }
