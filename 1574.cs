09ea95085859c248b98c84fed234eedd55817bac
Jodyisnotavailable <124157532+Jodyisnotavailable@users.noreply.github.com>
2024-11-15 19:44:41 +0800

    1574. Shortest Subarray to be Removed to Make Array Sorted

---
+++ 1574.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public int FindLengthOfShortestSubarray(int[] arr) {
+        int lPt = 0;
+        int rPt = arr.Length-1;
+
+        //remove prefix
+        while (rPt > 0 && arr[rPt] >= arr[rPt-1]) {
+            rPt -=1;
+        }
+        int res = rPt;
+
+        //suffix or middle
+        rPt = arr.Length-1;
+        while (rPt > lPt) {
+            while (rPt < arr.Length && rPt > lPt+1 && arr[rPt] >= arr[rPt-1] && arr[rPt] >= arr[lPt]) {
+                rPt -= 1;
+            }
+            //make sure prefix < suffix
+            while (rPt < arr.Length && arr[lPt] > arr[rPt]) {
+                rPt += 1;
+            }
+            res = Math.Min(res, rPt-lPt-1);
+            if (arr[lPt] > arr[lPt+1]) break;
+            lPt += 1;
+        }
+
+        return res;
+    }
+}
