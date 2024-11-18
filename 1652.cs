a17f800ca2f1c06a06066e8305d6641f26a7f52a
Jodyisnotavailable <124157532+Jodyisnotavailable@users.noreply.github.com>
2024-11-18 18:28:25 +0800

    1652. Defuse the Bomb

--- 1652.cs
+++ 1652.cs
@@ -3,6 +3,40 @@ public class Solution {
         int[] res = new int[code.Length];
         if (k == 0) return res;
 
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+        
+        //window
+        if (k > 0) {
+            start = 1;
+            end = k;
+        } else {
+            //previous elements "prefix"
+            start = (k+code.Length) % code.Length;
+            end = code.Length - 1;
+        }
+        
+        for (int i = start; i <= end; i ++) {
+            sum += code[i];
+        }
+
+        //update res and then update sum and window
+        for (int i = 0; i < code.Length; i ++) {
+            res[i] = sum;
+            sum -= code[(start) % code.Length];
+            sum += code[(end + 1) % code.Length];
+            start += 1;
+            end += 1;
+        }
+        
+        return res;
+        
+        //brute force
+        /*
+        int[] res = new int[code.Length];
+        if (k == 0) return res;
+
         for (int lP = 0; lP < code.Length; lP ++) {
             if (k > 0) {
                 for (int rP = lP+1; rP-lP <= k; rP ++) {
@@ -19,5 +53,6 @@ public class Solution {
         }
         
         return res;
+        */
     }
 }
